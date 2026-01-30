baa789858ae56623ad4eae2e9199910e9cdb0870
Ferdi Dirgantara <ferdifordev@gmail.com>
2026-01-30 11:17:36 +0800

    Add deploy workflow and set base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://frdid.github.io',
+  base: '/afirel-shop',
   vite: {
     plugins: [tailwindcss()]
   }
