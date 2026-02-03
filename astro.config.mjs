4c201073cfa1cc7f3e8990e4dc348ea7e4ba16a3
Ferdi Dirgantara <ferdifordev@gmail.com>
2026-02-03 11:37:46 +0800

    Fix: base url and product card's button

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://frdid.github.io',
-  base: '/afirel-shop/',
+  site: 'https://www.afirel.com',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [mdx()]
 });
